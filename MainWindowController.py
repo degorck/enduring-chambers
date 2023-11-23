b1c621fa68f8d50ad896e079ff323c71df7a2fac
Degorck <degorckterr@gmail.com>
2023-11-23 12:22:45 -0600

    Login window working

--- MainWindowController.py
+++ MainWindowController.py
@@ -12,27 +12,28 @@ class MainWindowController(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindowController, self).__init__()
         self.setupUi(self)
-        self.__user_type_key = UserTypeKey.GUEST
+        self.__user_type_key = UserTypeKey.GUEST.value
         self.__login_controller = LoginController()
         self.__create_user_controller = CreateUserController()
         self.__search_user_controller = SearchUserController()
         self.push_button_create_user.clicked.connect(self.__create_user_controller.show)
         self.push_button_search_users.clicked.connect(self.__search_user_controller.show)
-        self.__login_controller.push_button_login.clicked.connect(self.configure_windows_by_user_type)
-        self.__login_controller.push_button_guest.clicked.connect(self.configure_windows_by_user_type)
+        self.__login_controller.push_button_login.clicked.connect(self.__configure_windows_by_user_type)
+        self.__login_controller.line_edit_password.returnPressed.connect(self.__configure_windows_by_user_type)
+        self.__login_controller.push_button_guest.clicked.connect(self.__configure_windows_by_user_type)
         self.show()
         self.__login_controller.show()
     
-    def configure_windows_by_user_type(self):
+    def __configure_windows_by_user_type(self):
         self.__user_type_key = self.__login_controller.get_logged_user_type_key()
         match self.__user_type_key:
-            case UserTypeKey.GUEST:
+            case UserTypeKey.GUEST.value:
                 self.__configure_guest_window()
                 print(self.__user_type_key)
-            case UserTypeKey.ADMINISTRATOR():
+            case UserTypeKey.ADMINISTRATOR.value:
                 self.__configure_administrator_window()
                 print(self.__user_type_key)
-            case UserTypeKey.CAPTURIST():
+            case UserTypeKey.CAPTURIST.value:
                 self.__configure_capturist_window()
                 print(self.__user_type_key)
 
@@ -51,23 +52,23 @@ class MainWindowController(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def __configure_capturist_window(self):
         self.push_button_create_user.setEnabled(False)
-        self.push_button_search_users.setEnabled(False)
-        self.push_button_create_holder.setEnabled(False)
-        self.push_button_search_holders.setEnabled(False)
-        self.push_button_create_deceased.setEnabled(False)
-        self.push_button_search_deceased.setEnabled(False)
-        self.push_button_create_niche.setEnabled(False)
-        self.push_button_search_niches.setEnabled(False)
+        self.push_button_search_users.setEnabled(True)
+        self.push_button_create_holder.setEnabled(True)
+        self.push_button_search_holders.setEnabled(True)
+        self.push_button_create_deceased.setEnabled(True)
+        self.push_button_search_deceased.setEnabled(True)
+        self.push_button_create_niche.setEnabled(True)
+        self.push_button_search_niches.setEnabled(True)
 
     def __configure_administrator_window(self):
-        self.push_button_create_user.setEnabled(False)
-        self.push_button_search_users.setEnabled(False)
-        self.push_button_create_holder.setEnabled(False)
-        self.push_button_search_holders.setEnabled(False)
-        self.push_button_create_deceased.setEnabled(False)
-        self.push_button_search_deceased.setEnabled(False)
-        self.push_button_create_niche.setEnabled(False)
-        self.push_button_search_niches.setEnabled(False)
+        self.push_button_create_user.setEnabled(True)
+        self.push_button_search_users.setEnabled(True)
+        self.push_button_create_holder.setEnabled(True)
+        self.push_button_search_holders.setEnabled(True)
+        self.push_button_create_deceased.setEnabled(True)
+        self.push_button_search_deceased.setEnabled(True)
+        self.push_button_create_niche.setEnabled(True)
+        self.push_button_search_niches.setEnabled(True)
 
 if __name__ == "__main__": 
     app = QtWidgets.QApplication(sys.argv)
