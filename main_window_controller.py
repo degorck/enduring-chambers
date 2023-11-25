91e17c7eb6633c4e2496387d60438ba540c212cd
Degorck <degorckterr@gmail.com>
2023-11-24 21:43:50 -0600

    Improved code checking pylinter

--- main_window_controller.py
+++ main_window_controller.py
@@ -1,17 +1,18 @@
+'''Module that controlls the MainWindow'''
 import sys
-import PySide6.QtCore as QtCore
-import PySide6.QtWidgets as QtWidgets
+from PySide6 import QtWidgets
 from niches.view.Ui_MainWindow import Ui_MainWindow
-from niches.controller.ErrorController import ErrorController
+#from niches.controller.ErrorController import ErrorController
 from niches.controller.CreateUserController import CreateUserController
 from niches.controller.SearchUserController import SearchUserController
-from niches.controller.LoginController import LoginController
+from niches.controller.login_controller import LoginController
 from niches.util.Constants import UserTypeKey
 from niches.util.LoggingConfiguration import get_loging
 
 logging = get_loging()
 
 class MainWindowController(QtWidgets.QMainWindow, Ui_MainWindow):
+    """Class with MainWindow functionality"""
     def __init__(self):
         super(MainWindowController, self).__init__()
         self.setupUi(self)
@@ -21,30 +22,28 @@ class MainWindowController(QtWidgets.QMainWindow, Ui_MainWindow):
         self.__search_user_controller = SearchUserController()
         self.push_button_create_user.clicked.connect(self.__create_user_controller.show)
         self.push_button_search_users.clicked.connect(self.__search_user_controller.show)
-        self.__login_controller.push_button_login.clicked.connect(self.__configure_windows_by_user_type)
-        self.__login_controller.line_edit_password.returnPressed.connect(self.__configure_windows_by_user_type)
-        self.__login_controller.push_button_guest.clicked.connect(self.__configure_windows_by_user_type)
+        self.__login_controller.push_button_login.clicked.connect(
+            self.__configure_windows_by_user_type)
+        self.__login_controller.line_edit_password.returnPressed.connect(
+            self.__configure_windows_by_user_type)
+        self.__login_controller.push_button_guest.clicked.connect(
+            self.__configure_windows_by_user_type)
         self.show()
         self.__login_controller.show()
         logging.info("El sistema ha iniciado")
-    
     def __configure_windows_by_user_type(self):
         self.__user_type_key = self.__login_controller.get_logged_user_type_key()
         match self.__user_type_key:
             case UserTypeKey.GUEST.value:
                 self.__configure_guest_window()
-
             case UserTypeKey.ADMINISTRATOR.value:
                 self.__configure_administrator_window()
-
             case UserTypeKey.CAPTURIST.value:
                 self.__configure_capturist_window()
-
             case UserTypeKey.NOT_LOGGED.value:
                 self.__configure_not_logged_window()
-        logging.debug("Se ha configurado la ventana de acuerdo al usuario loggeado: " + self.__user_type_key)
-
-        
+        logging.debug(
+            "Se ha configurado la ventana de acuerdo al usuario loggeado: " + self.__user_type_key)
     def __configure_guest_window(self):
         self.push_button_create_user.setEnabled(False)
         self.push_button_search_users.setEnabled(True)
@@ -54,7 +53,6 @@ class MainWindowController(QtWidgets.QMainWindow, Ui_MainWindow):
         self.push_button_search_deceased.setEnabled(True)
         self.push_button_create_niche.setEnabled(False)
         self.push_button_search_niches.setEnabled(True)
-
     def __configure_capturist_window(self):
         self.push_button_create_user.setEnabled(False)
         self.push_button_search_users.setEnabled(True)
@@ -64,7 +62,6 @@ class MainWindowController(QtWidgets.QMainWindow, Ui_MainWindow):
         self.push_button_search_deceased.setEnabled(True)
         self.push_button_create_niche.setEnabled(True)
         self.push_button_search_niches.setEnabled(True)
-
     def __configure_administrator_window(self):
         self.push_button_create_user.setEnabled(True)
         self.push_button_search_users.setEnabled(True)
@@ -74,7 +71,6 @@ class MainWindowController(QtWidgets.QMainWindow, Ui_MainWindow):
         self.push_button_search_deceased.setEnabled(True)
         self.push_button_create_niche.setEnabled(True)
         self.push_button_search_niches.setEnabled(True)
-    
     def __configure_not_logged_window(self):
         self.push_button_create_user.setEnabled(False)
         self.push_button_search_users.setEnabled(False)
@@ -84,10 +80,9 @@ class MainWindowController(QtWidgets.QMainWindow, Ui_MainWindow):
         self.push_button_search_deceased.setEnabled(False)
         self.push_button_create_niche.setEnabled(False)
         self.push_button_search_niches.setEnabled(False)
-
     def get_logged_user_type_key(self):
+        '''Returns user_type_key'''
         return self.__user_type_key
-
 if __name__ == "__main__": 
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindowController()
--- niches/controller/error_controller.py
+++ niches/controller/error_controller.py
@@ -1,20 +1,20 @@
-import sys
-import PySide6.QtCore as QtCore
-import PySide6.QtWidgets as QtWidgets
+'''Module that controls error window'''
+from PySide6 import QtWidgets
 from niches.view.Ui_Error import Ui_Error
 
 class ErrorController(QtWidgets.QWidget, Ui_Error):
+    '''Class with error window functionality'''
     def __init__(self):
         super(ErrorController, self).__init__()
         self.setupUi(self)
         self.label_error_description.setText("")
-        self.push_button_accept.clicked.connect(self.hide_window)
-    
+        self.push_button_accept.clicked.connect(self.__hide_window)
     def handle_value_error(self, ve:ValueError):
+        '''Loads value error on label_error_description'''
         self.label_error_description.setText(str(ve))
-
     def handle_exception_error(self, ex:Exception):
+        '''Loads exception error on label_error_description'''
         self.label_error_description.setText(str(ex))
-    
-    def hide_window(self):
+    def __hide_window(self):
         self.close()
+        
