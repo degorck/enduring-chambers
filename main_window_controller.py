f02927d727e2c1eb73048d044a1228924e8b7583
Degorck <degorckterr@gmail.com>
2023-11-26 10:04:19 -0600

    controller package adjusted to pylint

--- main_window_controller.py
+++ main_window_controller.py
@@ -1,10 +1,13 @@
-'''Module that controlls the MainWindow'''
+
+"""
+Module that controls the MainWindow
+"""
 import sys
 from PySide6 import QtWidgets
 from niches.view.Ui_MainWindow import Ui_MainWindow
 #from niches.controller.ErrorController import ErrorController
-from niches.controller.CreateUserController import CreateUserController
-from niches.controller.SearchUserController import SearchUserController
+from niches.controller.create_user_controller import CreateUserController
+from niches.controller.search_user_controller import SearchUserController
 from niches.controller.login_controller import LoginController
 from niches.util.Constants import UserTypeKey
 from niches.util.LoggingConfiguration import get_loging
@@ -12,7 +15,13 @@ from niches.util.LoggingConfiguration import get_loging
 logging = get_loging()
 
 class MainWindowController(QtWidgets.QMainWindow, Ui_MainWindow):
-    """Class with MainWindow functionality"""
+    """
+    Class with the functionality of MainWindow
+
+    Args:
+        QtWidgets (QtWidgets.QMainWindow): Core QMainWindow
+        Ui_MainWindow (Ui_MainWindow): Qt layer transformed to python code 
+    """
     def __init__(self):
         super(MainWindowController, self).__init__()
         self.setupUi(self)
@@ -31,6 +40,7 @@ class MainWindowController(QtWidgets.QMainWindow, Ui_MainWindow):
         self.show()
         self.__login_controller.show()
         logging.info("El sistema ha iniciado")
+
     def __configure_windows_by_user_type(self):
         self.__user_type_key = self.__login_controller.get_logged_user_type_key()
         match self.__user_type_key:
@@ -44,6 +54,7 @@ class MainWindowController(QtWidgets.QMainWindow, Ui_MainWindow):
                 self.__configure_not_logged_window()
         logging.debug(
             "Se ha configurado la ventana de acuerdo al usuario loggeado: " + self.__user_type_key)
+
     def __configure_guest_window(self):
         self.push_button_create_user.setEnabled(False)
         self.push_button_search_users.setEnabled(True)
@@ -53,6 +64,7 @@ class MainWindowController(QtWidgets.QMainWindow, Ui_MainWindow):
         self.push_button_search_deceased.setEnabled(True)
         self.push_button_create_niche.setEnabled(False)
         self.push_button_search_niches.setEnabled(True)
+
     def __configure_capturist_window(self):
         self.push_button_create_user.setEnabled(False)
         self.push_button_search_users.setEnabled(True)
@@ -62,6 +74,7 @@ class MainWindowController(QtWidgets.QMainWindow, Ui_MainWindow):
         self.push_button_search_deceased.setEnabled(True)
         self.push_button_create_niche.setEnabled(True)
         self.push_button_search_niches.setEnabled(True)
+
     def __configure_administrator_window(self):
         self.push_button_create_user.setEnabled(True)
         self.push_button_search_users.setEnabled(True)
@@ -71,6 +84,7 @@ class MainWindowController(QtWidgets.QMainWindow, Ui_MainWindow):
         self.push_button_search_deceased.setEnabled(True)
         self.push_button_create_niche.setEnabled(True)
         self.push_button_search_niches.setEnabled(True)
+
     def __configure_not_logged_window(self):
         self.push_button_create_user.setEnabled(False)
         self.push_button_search_users.setEnabled(False)
@@ -80,10 +94,18 @@ class MainWindowController(QtWidgets.QMainWindow, Ui_MainWindow):
         self.push_button_search_deceased.setEnabled(False)
         self.push_button_create_niche.setEnabled(False)
         self.push_button_search_niches.setEnabled(False)
+
     def get_logged_user_type_key(self):
-        '''Returns user_type_key'''
+        """
+        Gets the __user_type_key of the logged user.
+
+        Returns:
+            __user_type_key : str
+                User type key of the logged user.
+        """
         return self.__user_type_key
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindowController()
     sys.exit(app.exec())
