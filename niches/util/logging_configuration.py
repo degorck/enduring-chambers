d3854d94dcdd768a0cea48a20acddc5ac0aa8791
Degorck <degorckterr@gmail.com>
2023-12-14 20:07:23 -0600

    Improved .env configuration

--- niches/util/logging_configuration.py
+++ niches/util/logging_configuration.py
@@ -3,12 +3,12 @@ Module configuration for logging
 """
 import logging
 import datetime
-from niches.constant.constants import LOG_FILENAME, LOGING_LEVEL
+from niches.constant.constants import LOG_FILENAME, LOGGING_LEVEL
 from niches.constant.constants import UTF_8, LOG_FORMAT, CONSOLE_LOG_ENABLED
 
 logging.basicConfig(filename = LOG_FILENAME + "-"+ str(datetime.date.today()) + ".log",
                     encoding = UTF_8,
-                    level = LOGING_LEVEL,
+                    level = LOGGING_LEVEL,
                     format = LOG_FORMAT.replace("\n", ""))
 if CONSOLE_LOG_ENABLED:
     handler = logging.StreamHandler()
